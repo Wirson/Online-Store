39d117da12b97abe69da2e40028ab10667954206
tulexx <tulexx@wp.pl>
2017-03-26 23:47:05 +0200

    Moved html to views

---
+++ src/views/admin_login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <div>
+            <h2>Admin</h2>
+            <h3>Log In</h3>
+            <form action="" method="post">
+                <input type="text" name="email" placeholder="Your E-mail">
+                <br>
+                <input type="password" name="password" placeholder="Your Password">
+                <br>
+                <input type="submit" value="Log In">
+            </form>
+        </div>
+    </body>
+</html>
--- www/index.php
+++ www/index.php
@@ -2,42 +2,27 @@
 use Shop\User;
 require_once __DIR__ . '/../bootstrap.php';
 //check user, head to main page
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $user = User::getByEmail($_POST['email']);//User::loadUserByEmail($_POST['email']);
-    if ($user != null) {
-        if ($user->passVerify($_POST['password'])) {
-            $_SESSION ['userId'] = $user->getId();
-            $_SESSION ['email'] = $user->getEmail();
-            $_SESSION ['name'] = $user->getName();
-            $_SESSION ['surname'] = $user->getSurname();
-            $_SESSION ['address'] = $user->getAddress();
-            //header("Location: index.php");            
-            echo 'eloe';
+if (!isset($_SESSION['user'])) {
+    require __DIR__ . '/../src/views/login.php';
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $user = User::getByEmail($_POST['email']);//User::loadUserByEmail($_POST['email']);
+        if ($user != null) {
+            if ($user->passVerify($_POST['password'])) {
+                $_SESSION['userId'] = $user->getId();
+                $_SESSION['email'] = $user->getEmail();
+                $_SESSION['name'] = $user->getName();
+                $_SESSION['surname'] = $user->getSurname();
+                $_SESSION['address'] = $user->getAddress();
+                $_SESSION['user'] = true;
+                //header("Location: index.php");            
+                echo 'eloe';
+            } else {
+                echo 'Wrong password!';
+            }
         } else {
-            echo 'Wrong password!';
+            echo "Wrong E-mail";
         }
-    } else {
-        echo "Wrong E-mail";
     }
+} else {
+    //require normal page
 }
-?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <div>
-            <h3>Log In</h3>
-            <form action="" method="post">
-                <input type="text" name="email" placeholder="Your E-mail">
-                <br>
-                <input type="password" name="password" placeholder="Your Password">
-                <br>
-                <input type="submit" value="Log In">
-            </form>
-            <a href="register.php"><button>Register</button></a>
-        </div>
-    </body>
-</html>
