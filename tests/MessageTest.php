41b603e4261e650725f8466280002e6e42f1095e
tulexx <tulexx@wp.pl>
2017-03-26 18:31:46 +0200

    Moved messages under users in dump and added message creation and deletion tests

---
+++ tests/MessageTest.php
@@ -0,0 +1,38 @@
+<?php
+
+use Shop\Message;
+
+/**
+ * @author tulexx
+ */
+class MessageTest extends \PHPUnit\DbUnit\TestCase
+{
+    protected function getConnection()
+    {
+        $conn = new PDO(DB_DSN, DB_USER, DB_PASS);
+        return new \PHPUnit\DbUnit\Database\DefaultConnection($conn, DB_NAME);
+    }
+
+    protected function getDataSet()
+    {
+        return $this->createMySQLXMLDataSet('./dump.xml');
+    }
+
+    public function testMessageCreation()
+    {
+        $message = new Message;
+        $message->setMessage('second testing message');
+        $message->setUserId(1);
+        $message->saveToDB();
+
+        $this->assertSame(2, $message->getId());
+    }
+
+    public function testMessageDeletion()
+    {
+        $message = Message::getById(1);
+        $message->delete();
+
+        $this->assertFalse(Message::getById(1));
+    }
+}
